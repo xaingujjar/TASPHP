f198e267012ae5fbfb8c3b0e892f650158da0035
xaingujjar <zainsajid266@gmail.com>
2024-08-12 18:13:31 +0500

    mm

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bootstrap</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container" style="color: white">
+    <div class="row">
+        <div class="col-12 col-md-8" style="background-color: #1c7430">.col-12 .col-md-8</div>
+        <div class="col-6 col-md-4" style="background-color: #1d1fa6">.col-6 .col-md-4</div>
+    </div>
+
+    <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+    <div class="row">
+        <div class="col-6 col-md-4" style="background-color: #a6f4aa">.col-6 .col-md-4</div>
+        <div class="col-6 col-md-4" style="background-color: #deb738">.col-6 .col-md-4</div>
+        <div class="col-6 col-md-4" style="background-color: #a6f4aa">.col-6 .col-md-4</div>
+    </div>
+
+    <!-- Columns are always 50% wide, on mobile and desktop -->
+    <div class="row">
+        <div class="col-6" style="background-color: #2c7760">.col-6</div>
+        <div class="col-6" style="background-color: #c14a4a">.col-6</div>
+    </div>
+</div>
+
+
+
+
+    <script src="js/bootstrap.min.js" ></script>
+</body>
+</html>
